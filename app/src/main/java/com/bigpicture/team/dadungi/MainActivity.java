7be8c2cadda4103a51a968c3d43163824444b6ca
NuuN <dndplok2002@naver.com>
2017-10-02 16:29:58 +0900

    프로젝트 이동간의 오타 및 오류 수정

--- app/src/main/java/com/bigpicture/team/dadungi/MainActivity.java
+++ app/src/main/java/com/bigpicture/team/dadungi/MainActivity.java
@@ -12,39 +12,89 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.bigpicture.team.dadungi.item.MemberInfoItem;
+import com.bigpicture.team.dadungi.lib.GoLib;
+import com.bigpicture.team.dadungi.lib.StringLib;
+import com.bigpicture.team.dadungi.remote.RemoteService;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private final String TAG = getClass().getSimpleName();
+    MemberInfoItem memberInfoItem;
+    DrawerLayout drawer;
+    View headerLayout;
+    CircleImageView profileIconImage;
+
+    //액티비티와 네이게이션 뷰를 설정하고 Fragment를 보여준다.
+    //savedInstanceState: 액티비티가 새로 생성되었을 경우에 이전 상태 값을 가지는 객체
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        memberInfoItem =((MyApp)getApplication()).getMemberInfoItem();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        headerLayout = navigationView.getHeaderView(0);
+
+        //GoLib.getInstance().goFragment(getSupportFragmentManager(), R.id.content_main,ESCListFragment.newInstance());
     }
+
+    //프로필 정보는 별도 액티비티에서 변경될 수 있으므로 변경을 바로 감지하기 위해서
+    //화면이 새로 보여질 때마다 setProfileView()를 호출
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setProfileView();
+    }
+
+    //프로필 이미지와 프로필 이름 설정
+
+    private void setProfileView(){
+        profileIconImage = (CircleImageView)headerLayout.findViewById(R.id.profile_icon);
+//        profileIconImage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                drawer.closeDrawer(GravityCompat.START);
+//                GoLib.getInstance().goProfileActivity(MainActivity.this);
+//            }
+//        });
+
+        TextView nameText = (TextView) headerLayout.findViewById(R.id.name);
+
+        if(memberInfoItem.nickname == null || memberInfoItem.nickname.equals("")){
+            nameText.setText(R.string.nickname_need);
+        }else {
+            nameText.setText(memberInfoItem.nickname);
+        }
+
+    }
+
+    //뒤로가기 키를 눌렀을 때 호출.
+    //네비게이션이 보이는 상태라면 네비게이션을 닫는다
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -52,27 +102,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -80,21 +109,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_list) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_map) {
+            GoLib.getInstance().goFragment(getSupportFragmentManager(),
+                    R.id.content_main, ESCMapFragment.newInstance());
+        } else if (id == R.id.nav_board) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_news) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_profile) {
+            GoLib.getInstance().goProfileActivity(this);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
