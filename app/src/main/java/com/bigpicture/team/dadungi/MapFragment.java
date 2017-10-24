f12a58710534a693ccc0483860c0e75e81749c6b
NuuN <dndplok2002@naver.com>
2017-10-24 19:08:55 +0900

    지도의 정보 프레그먼트 창이 띄워진 채로 취소키 눌렀을 시에 프레그먼트 창을 닫음

--- app/src/main/java/com/bigpicture/team/dadungi/MapFragment.java
+++ app/src/main/java/com/bigpicture/team/dadungi/MapFragment.java
@@ -14,6 +14,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -112,6 +113,23 @@ public class MapFragment extends Fragment
 //        memberId = ((MyApp)this.getActivity().getApplication()).getMemberId();
 
         View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        v.setFocusableInTouchMode(true);
+        v.requestFocus();
+        v.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ( keyCode == KeyEvent.KEYCODE_BACK && isMarkerClicked){
+                    ft = fm2.beginTransaction();
+                    ft.remove(mapInfoFragment);
+                    ft.commit();
+                    isMarkerClicked = false;
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
         return v;
     }
 
